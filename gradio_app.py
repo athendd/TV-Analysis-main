28285114d1b144a493ba70e371bcf16c374cbe27
athendd <141829395+athendd@users.noreply.github.com>
2025-06-12 19:33:43 -0400

    "built character chatbot"

--- gradio_app.py
+++ gradio_app.py
@@ -2,6 +2,7 @@ import gradio as gr
 from theme_classifier import ThemeClassifier
 from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
 from text_classification import JutsuClassifier
+from character_chatbot import CharacterChatBot
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -51,6 +52,14 @@ def classify_text(text_classification_model, text_classification_data_path, text
     
     return output
 
+def chat_with_character_chatbot(message, history):
+    character_chatbot = CharacterChatBot('athynne/Naruto_Llama-3-8B', huggingface_token = os.getenv('huggingface_token'))
+    
+    output = character_chatbot.chat(message, history)
+    output = output['content'].strip()
+    
+    return output
+
 def main():
     with gr.Blocks() as iface:
         with gr.Row():
@@ -92,6 +101,12 @@ def main():
                         ner_path = gr.Textbox(label = "NERs Save Path")
                         get_network_graph_button = gr.Button("Get Character Network")
                         get_network_graph_button.click(get_character_network, inputs = [subtitles_path, ner_path], outputs = [network_html])
+                        
+        #Character chatbot section
+        with gr.Row():
+            with gr.Column():
+                gr.HTML('<h1>Character Chatbot</h1>')
+                gr.ChatInterface(chat_with_character_chatbot)
                             
     iface.launch(share = True)
                 
