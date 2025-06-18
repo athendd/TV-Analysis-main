0690b1a688379213051def6aa22c13aded8e7c9a
athendd <141829395+athendd@users.noreply.github.com>
2025-06-18 19:56:34 -0400

    "starting adding hunter x hunter data to data loader'

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,14 +1,13 @@
 from glob import glob
 import pandas as pd
+import os
 
 def load_subtitles_dataset(dataset_path):
     subtitles_paths = glob(dataset_path+'/*.ass')
 
     scripts=[]
     episode_num=[]
-
     for path in subtitles_paths:
-
         #Read Lines
         with open(path,'r', encoding = 'utf-8') as file:
             lines = file.readlines()
@@ -22,6 +21,41 @@ def load_subtitles_dataset(dataset_path):
 
         scripts.append(script)
         episode_num.append(episode)
-
+        
     df = pd.DataFrame.from_dict({"episode":episode_num, "script":scripts })
     return df
+
+def load_hunterxhunter_subtitles_dataset(dataset_path):
+    scripts_total = []
+    episode_num_total = []
+    directories = []
+    
+    for directory in os.listdir(dataset_path):
+        if not directory.endswith('.txt'):
+            directories.append(directory)
+            
+    for directory in directories:
+        directory_path = dataset_path + f'/{directory}'
+        subtitles_path = glob(directory_path + '/*.txt')
+        
+        scripts = []
+        episode_num = []
+            
+        for path in subtitles_path:
+            with open(path, 'r', encoding = 'utf-8') as file:
+                lines = file.readlines()
+                
+            script = ' '.join(lines)
+            episode = path.split('\\')[-1].split(' ')[-1].split('.')[0]
+            
+            scripts.append(script)
+            episode_num.append(episode)
+        
+        scripts_total.extend(script)
+        episode_num_total.extend(episode_num)
+    
+    print(scripts_total)
+    print(episode_num_total)
+    
+#load_hunterxhunter_subtitles_dataset(r'Data\HunterxHunterSubtitles')
+load_subtitles_dataset(r'Data\Subtitles')
