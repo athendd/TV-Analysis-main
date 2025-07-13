e76287f93869718a541560ea742c9797fc481963
athendd <141829395+athendd@users.noreply.github.com>
2025-07-13 17:44:51 -0400

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,28 +3,27 @@ import pandas as pd
 import os
 import re   
 
-def load_subtitles_dataset(dataset_path):
-    subtitles_paths = glob(dataset_path+'/*.ass')
-
-    scripts=[]
-    episode_num=[]
-    for path in subtitles_paths:
-        with open(path,'r', encoding = 'utf-8') as file:
-            lines = file.readlines()
-            lines = lines[27:]
-            lines =  [ ",".join(line.split(',')[9:])  for line in lines ]
-        
-        lines = [ line.replace('\\N',' ') for line in lines]
-        script = " ".join(lines)
-
-        episode = int(path.split('-')[-1].split('.')[0].strip())
-
-        scripts.append(script)
-        episode_num.append(episode)
-        break
+def episode_cleaner(lines):
+    #Checks to see if x appears twice in line both times by itself which is how episode titles for HunterxHunter are spelled
+    pattern = r'\b[^x]*\s+x\s+[^x]*\s+x\s+[^x]*\b'
     
-    df = pd.DataFrame.from_dict({"episode":episode_num, "script":scripts })
-    return df
+    if lines[0].startswith('Fearsome monsters'):
+        lines = lines[9:]
+    
+    cleaned_lines = []
+    
+
+    for line in lines:
+        line = line.strip()
+        match = re.search(pattern, line)
+        
+        if "Gon and Killua's Hunterpedia" in line or "G.I. Tutorial" in line:
+            break
+        
+        if not line.startswith('Next time:') and not '[' in line and not match:
+            cleaned_lines.append(line)
+            
+    return cleaned_lines
 
 def load_hunterxhunter_arc_subtitles(directory, directory_path, scripts, episode_num):
     subtitles_path = glob(directory_path + '/*.txt')
@@ -36,8 +35,10 @@ def load_hunterxhunter_arc_subtitles(directory, directory_path, scripts, episode
         #Remove the first 8 lines from file since its just narration     
         if directory == 'Hunter Exam Arc':
             lines = lines[8:]  
+            
+        cleaned_lines = episode_cleaner(lines)
         
-        script = ' '.join(lines)
+        script = '\n '.join(cleaned_lines)
         episode = int(path.split('\\')[-1].split(' ')[-1].split('.')[0])
 
         scripts.append(script)
@@ -53,21 +54,7 @@ def load_hunterxhunter_single_episode(dataset_path):
         if episode_num >= 1 and episode_num < 22:
             lines = lines[8:]
         
-    
-    #Checks to see if x appears twice in line both times by itself which is how episode titles for HunterxHunter are spelled
-    pattern = r'\b[^x]*\s+x\s+[^x]*\s+x\s+[^x]*\b'
-    
-    cleaned_lines = []
-
-    for line in lines:
-        line = line.strip()
-        match = re.search(pattern, line)
-        
-        if "Gon and Killua's Hunterpedia" in line or "G.I. Tutorial" in line:
-            break
-        
-        if not line.startswith('Next time:') and not '[' in line and not match:
-            cleaned_lines.append(line)
+    cleaned_lines = episode_cleaner(lines)
                 
     return '\n'.join(cleaned_lines)
         
